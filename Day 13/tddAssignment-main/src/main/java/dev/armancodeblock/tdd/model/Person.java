ed2f8d738edf53d44c8cd0813ca0ff0f73dc5f2b
jspreeti <pyadav2007@gmail.com>
2024-06-19 12:27:40 -0400

    Assignment for the Candidate

---
+++ Day 13/tddAssignment-main/src/main/java/dev/armancodeblock/tdd/model/Person.java
@@ -0,0 +1,54 @@
+package dev.armancodeblock.tdd.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String mobile;
+
+    public Person(){
+
+    }
+
+    public Person(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    public Person(Integer id, String name, String mobile) {
+        this.id = id;
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
